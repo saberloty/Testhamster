57c8bd1df10ad9a9a6aec279c92535f2c6cc601e
saberloty <sabertayyeb@gmail.com>
2024-08-30 18:55:38 +0330

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,65 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const EVENTS_DELAY = 20000;
+
+  const games = {
+    1: {
+      name: "Riding Extreme 3D",
+      appToken: "d28721be-fd2d-4b45-869e-9f253b554e50",
+      promoId: "43e35910-c168-4634-ad4f-52fd764a843f",
+      timing: 30000, // 30 seconds
+      attempts: 25,
+    },
+    2: {
+      name: "Chain Cube 2048",
+      appToken: "d1690a07-3780-4068-810f-9b5bbf2931b2",
+      promoId: "b4170868-cef0-424f-8eb9-be0622e8e8e3",
+      timing: 30000, // 30 seconds
+      attempts: 20,
+    },
+    3: {
+      name: "My Clone Army",
+      appToken: "74ee0b5b-775e-4bee-974f-63e7f4d5bacb",
+      promoId: "fe693b26-b342-4159-8808-15e3ff7f8767",
+      timing: 180000, // 180 seconds
+      attempts: 30,
+    },
+    4: {
+      name: "Train Miner",
+      appToken: "82647f43-3f87-402d-88dd-09a90025313f",
+      promoId: "c4480ac7-e178-4973-8061-9ed5b2e17954",
+      timing: 30000, // 30 seconds
+      attempts: 15,
+    },
+    5: {
+      name: "Merge Away",
+      appToken: "8d1cc2ad-e097-4b86-90ef-7a27e19fb833",
+      promoId: "dc128d28-c45b-411c-98ff-ac7726fbaea4",
+      timing: 30000, // 30 seconds
+      attempts: 25,
+    },
+    6: {
+      name: "Twerk Race 3D",
+      appToken: "61308365-9d16-4040-8bb0-2f4a4c69074c",
+      promoId: "61308365-9d16-4040-8bb0-2f4a4c69074c",
+      timing: 30000, // 30 seconds
+      attempts: 20,
+    },
+    7: {
+      name: "Polysphere",
+      appToken: "2aaf5aee-2cbc-47ec-8a3f-0962cc14bc71",
+      promoId: "2aaf5aee-2cbc-47ec-8a3f-0962cc14bc71",
+      timing: 20000, // 20 seconds
+      attempts: 20,
+    },
+    8: {
+      name: "Mow and Trim",
+      appToken: "ef319a80-949a-492e-8ee0-424fb5fc20a6",
+      promoId: "ef319a80-949a-492e-8ee0-424fb5fc20a6",
+      timing: 20000, // 20 seconds
+      attempts: 20,
+    },
+    9: {
+      name: "Mud Racing",
       appToken: "8814a785-97fb-4177-9193-ca4180ff9da8",
       promoId: "8814a785-97fb-4177-9193-ca4180ff9da8",
       timing: 20000, // 20 seconds
@@ -77,11 +139,11 @@
     document.querySelector(".grid-container").style.display = "none";
     keyCountGroup.classList.add("hidden");
 
-    keyCountLabel.innerText = `تعداد کلیدها : ${keyCount}`;
+    keyCountLabel.innerText = `ŲŖŲ¹ŲÆŲ§ŲÆ Ś©Ł„ŪŲÆŁ‡Ų§ : ${keyCount}`;
 
     progressBar.style.width = "0%";
     progressText.innerText = "0%";
-    progressLog.innerText = "شروع ...";
+    progressLog.innerText = "Ų´Ų±ŁŲ¹ ...";
     progressContainer.classList.remove("hidden");
     keyContainer.classList.add("hidden");
     generatedKeysTitle.classList.add("hidden");
@@ -94,7 +156,7 @@
     const updateProgress = (increment, message) => {
       progress += increment;
       progressBar.style.width = `${progress}%`;
-      progressText.innerText = `${progress}%`;
+      progressText.innerText = `${progress.toFixed(2)}%`;
       progressLog.innerText = message;
     };
 
@@ -109,25 +171,21 @@
         return null;
       }
 
-      for (let i = 0; i < 20; i++) {
-     
+      for (let i = 0; i < game.attempts; i++) {
         const hasCode = await emulateProgress(clientToken, game.promoId);
-        updateProgress(5 / keyCount, "شبیه سازی فرآیند ...");
+        updateProgress(
+          100 / game.attempts / keyCount,
+          ` ... ${i + 1}/${game.attempts} ŲŖŲ¹ŲÆŲ§ŲÆ ŲŖŁ„Ų§Ų´`
+        );
         if (hasCode) {
           break;
         }
-
-        if (game.promoId == "fe693b26-b342-4159-8808-15e3ff7f8767") {
-          await sleep(120000);
-        } else {
-          await sleep(EVENTS_DELAY);
-        }
-
+        await sleep(game.timing); // Sleep after each attempt to wait before the next event registration
       }
 
       try {
         const key = await generateKey(clientToken, game.promoId);
-        updateProgress(7 / keyCount, " تولید کلید ...");
+        updateProgress(7 / keyCount, " ŲŖŁŁ„ŪŲÆ Ś©Ł„ŪŲÆ ...");
         return key;
       } catch (error) {
         alert(`Failed to generate key: ${error.message}`);
@@ -146,7 +204,7 @@
           (key) =>
             `<div class="key-item">
                     <input type="text" value="${key}" readonly>
-                    <button class="copyKeyBtn" data-key="${key}">کپی کلید</button>
+                    <button class="copyKeyBtn" data-key="${key}">Ś©Ł¾Ū Ś©Ł„ŪŲÆ</button>
                 </div>`
         )
         .join("");
@@ -154,7 +212,7 @@
     } else if (keys.length === 1) {
       keysList.innerHTML = `<div class="key-item">
                     <input type="text" value="${keys[0]}" readonly>
-                    <button class="copyKeyBtn" data-key="${keys[0]}">کپی کلید</button>
+                    <button class="copyKeyBtn" data-key="${keys[0]}">Ś©Ł¾Ū Ś©Ł„ŪŲÆ</button>
                 </div>`;
     }
 
